4a7c43aac629937f25fda0c0aac0c00a068fb575
Clovicorn <109252472+Clovicorn@users.noreply.github.com>
2023-04-15 23:08:03 -0700

    changed CharCreation

--- states/characterCreationState.cpp
+++ states/characterCreationState.cpp
@@ -20,24 +20,9 @@ namespace Clovicorn
         _data->assets.loadTexture("ccMenuBG", CCMENU_BACKGROUND_FILEPATH);
         _data->assets.loadTexture("player", TEST_PLAYER);
 
-        _data->assets.loadTexture("minusDown", MINUS_DOWN_FILEPATH);
-        _data->assets.loadTexture("minusOver", MINUS_OVER_FILEPATH);
-        _data->assets.loadTexture("minusUp", MINUS_UP_FILEPATH);
-
-        _data->assets.loadTexture("plusDown", PLUS_DOWN_FILEPATH);
-        _data->assets.loadTexture("plusOver", PLUS_OVER_FILEPATH);
-        _data->assets.loadTexture("plusUp", PLUS_UP_FILEPATH);
-
         _data->player.setSprite(_data->assets.getTexture("player"));
         _data->player.setPosition(Vector2f(_data->window.getSize().x / 2, _data->window.getSize().y / 2));
 
-        for (int x = 0; x < 12; x += 2)
-        {
-            _addSubSprites[x].setTexture(_data->assets.getTexture("plusUp"));
-            _addSubSprites[x + 1].setTexture(_data->assets.getTexture("minusUp"));
-            _addSubSprites[x].setPosition(150, (42 + (x * 42)));
-            _addSubSprites[x + 1].setPosition(150, 42 + 28 + (x * 42));
-        }
         _bgSprite.setTexture(_data->assets.getTexture("ccMenuBG"));
         _bgSprite.setScale(Vector2f(_currentWidthPercent, _currentHeightPercent));
         _bgSprite.setPosition(Vector2f(0, 0));
@@ -87,10 +72,7 @@ namespace Clovicorn
         _data->window.clear();
         _data->window.setView(_hudView);
         _data->window.draw(_bgSprite);
-        for (int x = 0; x < 12; x++)
-        {
-            _data->window.draw(_addSubSprites[x]);
-        }
+
         _data->window.draw(_data->player.getSprite());
         _data->window.display();
     }
